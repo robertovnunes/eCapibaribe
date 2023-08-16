e3b67bde067b5f3bbd6bd33e02e55bf122bb817e
koga25 <66087274+koga25@users.noreply.github.com>
2023-08-16 19:43:33 -0300

    feat: testing delete and modify user route implementations

--- backend/src/features/users/delete_user.py
+++ backend/src/features/users/delete_user.py
@@ -12,13 +12,17 @@ def save_json(path: str, object: dict):
     with open(path, 'w', encoding='utf-8') as json_file:
         json.dump(object, json_file)
 
-def delete_user_by_cpf(user_cpf: str) -> str:
-    data = read_json("./features/users/data/users.json")
+def delete_user_by_cpf(user_cpf: str, file_name: str) -> str:
+    if file_name == None:
+        file_name = "users"
+    
+    path = f"./features/users/data/{file_name}.json"
+    data = read_json(path)
     users: list[dict] = data["users"]
     for idx, user in enumerate(users):
         if user_cpf == user["cpf"]:
             users.pop(idx)
-            save_json("./features/users/data/users.json", data)            
+            save_json(path, data)            
             return "Usuário deletado com sucesso"
     
-    raise HTTPException(status_code=404, detail="User not found")
+    raise HTTPException(status_code=404, detail="Usuário não existe")
--- backend/src/features/users/user.py
+++ backend/src/features/users/user.py
@@ -7,15 +7,15 @@ from .delete_user import delete_user_by_cpf
 users_router = APIRouter()
 
 @users_router.post("/users/modify")
-async def create_item(user: User):
+async def create_item(user: User, file_name: str | None = None):
     success_message = modify_user(user)
     return {
         "message": success_message
     }
 
 @users_router.delete("/users/delete/{cpf}")
-async def delete_user(cpf: str):
-    success_message = delete_user_by_cpf(cpf)
+async def delete_user(cpf: str, file_name: str | None = None):
+    success_message = delete_user_by_cpf(cpf, file_name)
     return {
         "message": success_message
     }
