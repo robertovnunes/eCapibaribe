6844c6d079ecab53eb8ff0979e5cdc3ff7beb8c7
koga25 <66087274+koga25@users.noreply.github.com>
2023-08-16 13:33:37 -0300

    adding cors

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from features.users.user import users_router
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(users_router)
