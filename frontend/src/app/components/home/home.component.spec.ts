3e1c4975fc1fa0e515ccf271398206c2f2aec0f1
koga25 <66087274+koga25@users.noreply.github.com>
2023-09-14 10:50:18 -0300

    fix: fixing conflicts that for some reason bugged the merge editor.

--- frontend/src/app/components/home/home.component.spec.ts
+++ frontend/src/app/components/home/home.component.spec.ts
@@ -1,6 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-<<<<<<<< HEAD:frontend/src/app/components/home/home.component.spec.ts
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
@@ -12,19 +11,6 @@ describe('HomeComponent', () => {
       declarations: [HomeComponent]
     });
     fixture = TestBed.createComponent(HomeComponent);
-========
-import { EditarComponent } from './editar.component';
-
-describe('EditarComponent', () => {
-  let component: EditarComponent;
-  let fixture: ComponentFixture<EditarComponent>;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [EditarComponent]
-    });
-    fixture = TestBed.createComponent(EditarComponent);
->>>>>>>> 26c6c9f2c1e4e008de0c480ccf9fa4bb2b1eafc1:frontend/src/app/features/items/pages/editar/editar.component.spec.ts
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- frontend/src/app/features/items/pages/editar/editar.component.spec.ts
+++ frontend/src/app/features/items/pages/editar/editar.component.spec.ts
@@ -1,18 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-<<<<<<<< HEAD:frontend/src/app/components/home/home.component.spec.ts
-import { HomeComponent } from './home.component';
-
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [HomeComponent]
-    });
-    fixture = TestBed.createComponent(HomeComponent);
-========
 import { EditarComponent } from './editar.component';
 
 describe('EditarComponent', () => {
@@ -24,7 +11,6 @@ describe('EditarComponent', () => {
       declarations: [EditarComponent]
     });
     fixture = TestBed.createComponent(EditarComponent);
->>>>>>>> 26c6c9f2c1e4e008de0c480ccf9fa4bb2b1eafc1:frontend/src/app/features/items/pages/editar/editar.component.spec.ts
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
